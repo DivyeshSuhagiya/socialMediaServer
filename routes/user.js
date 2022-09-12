d7d237d5479f0cb4b7ea65b7da92b148cd67c693
DSTechnical-09 <91273646+DSTechnical-09@users.noreply.github.com>
2022-09-12 11:11:21 +0530

    change route api method

--- routes/user.js
+++ routes/user.js
@@ -11,7 +11,7 @@ router.post('/update' ,upload, (req,res) => userController.user.update(req,res))
 router.post('/login', (req,res) => userController.user.login(req,res));
 router.post('/followRequest', (req,res) => userController.user.followRequest(req,res));
 router.post('/followAccept', (req,res) => userController.user.followAccept(req,res));
-router.post('/followDelete', (req,res) => userController.user.followDelete(req,res));
+router.delete('/followDelete', (req,res) => userController.user.followDelete(req,res));
 router.post('/unfollow', (req,res) => userController.user.unFollow(req,res));
 
 module.exports = router
